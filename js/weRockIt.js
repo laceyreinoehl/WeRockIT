378c74bbaed2678394b8ef77c0bea2057270672f
lacey <lacey@lojix.com>
2018-05-20 19:51:45 -0500

    Added success messages to form submissions and made the image on the home page responsive

--- js/weRockIt.js
+++ js/weRockIt.js
@@ -3,29 +3,37 @@ $(function () {
     $(document).on('submit', '#test-form', submitToMailingList);
     $(document).on('submit', '#sponsorshipEmailForm', submitSponsorForm);
 
+    /**
+     *
+     * @param event
+     */
     function submitToMailingList(event) {
         event.preventDefault();
         var form = $('form#test-form'),
             url = 'https://script.google.com/macros/s/AKfycbz0nWMxmjxxguJski_acVi0VDUZBtDAGBxRhIzcjZU65Tedo9I/exec';
         $.get(url, form.serialize())
-            .done(function (response) {
-                debugger;
+            .done(function () {
+                $("#mailingListThankYou").slideDown();
             })
             .fail(function () {
-                debugger;
+                $("#mailingListError").slideDown();
             });
     }
 
+    /**
+     * 
+     * @param event
+     */
     function submitSponsorForm(event){
         event.preventDefault();
         var form = $('form#sponsorshipEmailForm'),
             url = 'https://script.google.com/macros/s/AKfycbzz7ZuItYGczN943eoRWZ3o1-qdv7a0lp27mIneYUm5pl4nBYix/exec';
         $.get(url, form.serialize())
-            .done(function (response) {
-                debugger;
+            .done(function () {
+                $("#sponsorInquiryFormThankYou").slideDown();
             })
             .fail(function () {
-                debugger;
+                $("#sponsorInquiryFormError").slideDown();
             });
     }
 });
